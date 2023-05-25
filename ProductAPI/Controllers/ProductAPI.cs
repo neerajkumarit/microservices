9b7cc15c4691939a031663f3060fc5c81fc94c31
Neeraj SINHA <neerajkumarit@gmail.com>
2023-05-25 15:59:50 +0530

    first commit

---
+++ ProductAPI/Controllers/ProductAPI.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductAPI.Models;
+using ProductAPI.Repository;
+
+namespace ProductAPI.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductAPI : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        protected ResponseDto _response;
+        public ProductAPI(IProductRepository productRepository)
+        {
+            _productRepository= productRepository;
+            this._response = new ResponseDto();
+        }
+
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDto> productDtos = await _productRepository.GetProducts();
+                _response.Result = productDtos;
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                     = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<object> Get(int id)
+        {
+            try
+            {
+                ProductDto productDto = await _productRepository.GetProductById(id);
+                _response.Result = productDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                     = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPost]       
+        public async Task<object> Post([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                ProductDto model = await _productRepository.CreateUpdateProduct(productDto);
+                _response.Result = model;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                     = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPut]      
+        public async Task<object> Put([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                ProductDto model = await _productRepository.CreateUpdateProduct(productDto);
+                _response.Result = model;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                     = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpDelete]       
+        [Route("{id}")]
+        public async Task<object> Delete(int id)
+        {
+            try
+            {
+                bool isSuccess = await _productRepository.DeleteProduct(id);
+                _response.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                     = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+    }
+}
---
+++ ProductAPI/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ProductAPI.Models;
+
+namespace ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>();
+                config.CreateMap<Product, ProductDto>();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
